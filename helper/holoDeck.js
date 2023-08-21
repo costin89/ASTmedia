3e1c957ba71f9ed1c46814a356ee89b61c2f1d71
Alexander Ast <mailtoast@googlemail.com>
2023-08-21 19:32:31 +0200

    Create holoDeck.js

---
+++ helper/holoDeck.js
@@ -0,0 +1,10 @@
+AFRAME.registerPrimitive('a-holoDeck', {
+  defaultComponents: {
+    geometry: {primitive: 'box', depth: '100', height: '100', width: '100'},
+    material: {color: 'black', side: 'double'}
+  },
+  mappings: {
+    // Hier können Sie Attribute hinzufügen, die auf Komponenten gemappt werden sollen. 
+    // Zum Beispiel könnten Sie `width` auf `geometry.width` abbilden, damit Sie die Breite direkt als Attribut angeben können.
+  }
+});
