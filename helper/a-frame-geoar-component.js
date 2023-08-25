02d5dee4a1b20c735e32ed491fd50c5396556d88
Alexander Ast <mailtoast@googlemail.com>
2023-08-25 16:03:36 +0200

    Update a-frame-geoar-component.js

--- helper/a-frame-geoar-component.js
+++ helper/a-frame-geoar-component.js
@@ -1,72 +1,61 @@
-// Datei: a-frame-geoar-component.js
-
+// a-frame-geoar-component.js
 AFRAME.registerComponent('a-geotag', {
     schema: {
         lat: {type: 'number', default: 0},
         long: {type: 'number', default: 0},
         getLocation: {type: 'boolean', default: false}
     },
-  
+
     init: function() {
         this.userLocation = { latitude: null, longitude: null };
         this.watchId = null;
+        
         if (this.data.getLocation) {
-            this.askForLocationPermission();
+            if (!('geolocation' in navigator)) {
+                console.warn('Geolocation API nicht verfügbar.');
+                return;
+            }
+
+            navigator.geolocation.getCurrentPosition(
+                (position) => this.onLocationUpdate(position),
+                (err) => console.warn('Fehler beim Abrufen der Geolocation: ', err),
+                {enableHighAccuracy: true}
+            );
         }
     },
 
-    askForLocationPermission: function() {
-        const component = this;
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                component.setUserLocation(position.coords.latitude, position.coords.longitude);
-                component.startWatchingLocation();
-            });
-        } else {
-            console.warn('Geolocation API not available in this browser.');
-        }
-    },
+    onLocationUpdate: function(position) {
+        this.userLocation.latitude = position.coords.latitude;
+        this.userLocation.longitude = position.coords.longitude;
+        console.log("Aktuelle Position: ", this.userLocation); // Zum Debuggen
+        
+        this.watchId = navigator.geolocation.watchPosition(
+            (position) => this.onLocationUpdate(position),
+            (err) => console.warn('Fehler beim Abrufen der Geolocation: ', err),
+            {enableHighAccuracy: true}
+        );
 
-    setUserLocation: function(lat, long) {
-        this.userLocation.latitude = lat;
-        this.userLocation.longitude = long;
         this.updateObjectPosition();
-    },
-
-    startWatchingLocation: function() {
-        const component = this;
-        this.watchId = navigator.geolocation.watchPosition(function(position) {
-            component.setUserLocation(position.coords.latitude, position.coords.longitude);
-        });
     },
 
     updateObjectPosition: function() {
         const distance = this.getDistanceFromLatLonInM(this.data.lat, this.data.long, this.userLocation.latitude, this.userLocation.longitude);
-        if (distance <= 250) {
-            // Objekt sichtbar machen
-            this.el.setAttribute('visible', true);
-            // Position des Objekts basierend auf Distanz und Richtung aktualisieren...
-            // Hier kann eine komplexere Umrechnung notwendig sein, um den Ort korrekt in der AR-Szene zu positionieren.
-        } else {
-            this.el.setAttribute('visible', false);
-        }
+        console.log("Entfernung zum Ziel: ", distance); // Zum Debuggen
+
+        this.el.setAttribute('visible', distance <= 250);
     },
 
     getDistanceFromLatLonInM: function(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius der Erde in km
+        var R = 6371; // Erdradius in km
         var dLat = this.deg2rad(lat2-lat1);
         var dLon = this.deg2rad(lon2-lon1); 
-        var a = 
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
-            Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c; // Distanz in km
-        return d * 1000; // Distanz in m
+        return R * c * 1000; // Entfernung in Metern
     },
 
     deg2rad: function(deg) {
-        return deg * (Math.PI/180)
+        return deg * (Math.PI/180);
     },
 
     remove: function() {
