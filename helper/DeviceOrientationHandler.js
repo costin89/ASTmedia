94272fde19bf8750a59e85e07b9e97ebc21d7b06
Alexander Ast <mailtoast@googlemail.com>
2023-08-28 17:18:46 +0200

    Update DeviceOrientationHandler.js

--- helper/DeviceOrientationHandler.js
+++ helper/DeviceOrientationHandler.js
@@ -7,16 +7,20 @@ class DeviceOrientationHandler {
   }
 
   async requestPermission() {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      const permission = await DeviceOrientationEvent.requestPermission();
-      if (permission === 'granted') {
-        this.permissionGranted = true;
-        window.addEventListener("deviceorientation", this.handleOrientation.bind(this), true);
+    if (window.DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === 'function') {
+      try {
+        const permission = await DeviceOrientationEvent.requestPermission();
+        if (permission === 'granted') {
+          this.permissionGranted = true;
+          window.addEventListener('deviceorientation', this.handleOrientation.bind(this));
+        }
+      } catch (error) {
+        console.error('Permission denied:', error);
       }
     } else {
       // Für Browser, die keine Berechtigungen benötigen
       this.permissionGranted = true;
-      window.addEventListener("deviceorientation", this.handleOrientation.bind(this), true);
+      window.addEventListener('deviceorientation', this.handleOrientation.bind(this));
     }
   }
 
@@ -26,3 +30,6 @@ class DeviceOrientationHandler {
     this.gamma = event.gamma;
   }
 }
+
+const deviceOrientationHandler = new DeviceOrientationHandler();
+document.getElementById('requestPermissionButton').addEventListener('click', () => deviceOrientationHandler.requestPermission());
