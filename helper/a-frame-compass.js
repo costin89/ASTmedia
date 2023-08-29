7bc5a5bfb51d34a07cbd57146263144ad8c76d6c
Alexander Ast <mailtoast@googlemail.com>
2023-08-29 15:21:41 +0200

    Update a-frame-compass.js

--- helper/a-frame-compass.js
+++ helper/a-frame-compass.js
@@ -1,35 +1,47 @@
-// a-frame-compass.js
 AFRAME.registerComponent('a-comp', {
-  init: async function() {
+  init: function() {
     const el = this.el;
+    
+    console.log("Initialisierung der a-comp Komponente.");
 
     // Funktion zum Setzen der y-Rotation der Box
     const setRotation = (alpha) => {
+      console.log("Setze Rotation: ", alpha);
       if (alpha !== null) {
         el.setAttribute('rotation', {y: alpha});
       }
     };
 
-    // Überprüfen, ob der Browser DeviceOrientationEvents unterstützt
-    if ('DeviceOrientationEvent' in window) {
-      // Für iOS 13 und höher
+    // Funktion zur Anforderung der Berechtigung für DeviceOrientationEvent
+    const requestPermission = async () => {
       if (typeof DeviceOrientationEvent.requestPermission === 'function') {
         const permission = await DeviceOrientationEvent.requestPermission();
-        if (permission === 'granted') {
-          window.addEventListener('deviceorientation', (event) => {
-            // Einmaligen Wert festlegen und dann den Event Listener entfernen
-            setRotation(event.alpha);
-            window.removeEventListener('deviceorientation', this);
-          });
-        }
-      } else {
-        // Für andere Browser/Versionen
-        window.addEventListener('deviceorientation', (event) => {
-          // Einmaligen Wert festlegen und dann den Event Listener entfernen
-          setRotation(event.alpha);
-          window.removeEventListener('deviceorientation', this);
-        });
+        console.log("Berechtigungsstatus: ", permission);
+        return permission === 'granted';
       }
+      console.log("Keine Berechtigungsanforderung erforderlich.");
+      return true;
+    };
+
+    // Überprüfen, ob der Browser DeviceOrientationEvents unterstützt
+    if ('DeviceOrientationEvent' in window) {
+      console.log("DeviceOrientationEvent wird unterstützt.");
+      
+      requestPermission().then(granted => {
+        if (granted) {
+          console.log("Berechtigung erteilt.");
+          const handler = (event) => {
+            console.log("DeviceOrientationEvent gefeuert.");
+            setRotation(event.alpha);
+            window.removeEventListener('deviceorientation', handler);
+          };
+          window.addEventListener('deviceorientation', handler);
+        } else {
+          console.log("Berechtigung nicht erteilt.");
+        }
+      });
+    } else {
+      console.log("DeviceOrientationEvent wird nicht unterstützt.");
     }
   }
 });
