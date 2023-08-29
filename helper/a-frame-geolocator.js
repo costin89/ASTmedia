c1c8ac6ffdff783a0a797e4ce73e0e0e32ac7a2a
Alexander Ast <mailtoast@googlemail.com>
2023-08-29 13:00:50 +0200

    Update a-frame-geolocator.js

--- helper/a-frame-geolocator.js
+++ helper/a-frame-geolocator.js
@@ -1,26 +1,25 @@
 // a-frame-geolocator.js
-AFRAME.registerPrimitive('a-geo', {
-  defaultComponents: {
-    'a-geo-comp': {}
-  },
-});
 
 AFRAME.registerComponent('a-geo-comp', {
   schema: {
-    latitude: {type: 'number', default: 0.0},
-    longitude: {type: 'number', default: 0.0},
-    deviation: {type: 'number', default: 0.0},
-    gps: {type: 'boolean', default: false} // Add gps property
+    latitude: { type: 'number', default: 0.0 },
+    longitude: { type: 'number', default: 0.0 },
+    deviation: { type: 'number', default: 0.0 },
+    gps: { type: 'boolean', default: false }
   },
 
   init: function() {
-    if (this.data.gps) { // Check if gps="true"
+    if (this.data.gps) {
       if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(this.onGeoSuccess.bind(this), this.onGeoError.bind(this), {
-          enableHighAccuracy: true
-        });
+        navigator.geolocation.getCurrentPosition(
+          position => this.onGeoSuccess(position),
+          error => this.onGeoError(error),
+          {
+            enableHighAccuracy: true
+          }
+        );
       } else {
-        console.warn('Geolocation is not available in this browser');
+        console.warn("Geolocation is not available in this browser");
       }
     }
   },
@@ -32,48 +31,51 @@ AFRAME.registerComponent('a-geo-comp', {
     const targetLong = this.data.longitude;
 
     const distance = this.computeHaversineDistance(userLat, userLong, targetLat, targetLong);
-    let heading = this.computeHeading(userLat, userLong, targetLat, targetLong);
+    let heading = this.computeHeading(userLat, userLong, targetLat, targetLong) - this.data.deviation;
+    const coords = this.convertToAFrameCoords(distance, heading);
 
-    heading -= this.toRadians(this.data.deviation);
-
-    const aFrameCoords = this.convertHeadingToAFrameCoordinates(distance, heading);
-    this.el.setAttribute('position', aFrameCoords);
+    this.el.object3D.position.set(coords.x, 0, coords.z);
   },
 
   onGeoError: function(error) {
-    console.warn('Geolocation error', error);
+    console.warn('Geolocation error: ', error);
   },
 
   computeHaversineDistance: function(lat1, lon1, lat2, lon2) {
-    const R = 6371000;
+    const R = 6371000; // Earth radius in meters
     const dLat = this.toRadians(lat2 - lat1);
     const dLon = this.toRadians(lon2 - lon1);
-    const a = 
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.toRadians(lat1)) * Math.cos(this.toRadians(lat2)) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.toRadians(lat1)) * Math.cos(this.toRadians(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
     return R * c;
   },
 
   computeHeading: function(lat1, lon1, lat2, lon2) {
     const dLon = this.toRadians(lon2 - lon1);
-    const lat1R = this.toRadians(lat1);
-    const lat2R = this.toRadians(lat2);
-
-    const y = Math.sin(dLon) * Math.cos(lat2R);
-    const x = Math.cos(lat1R) * Math.sin(lat2R) -
-              Math.sin(lat1R) * Math.cos(lat2R) * Math.cos(dLon);
-    return Math.atan2(y, x);
+    const y = Math.sin(dLon) * Math.cos(this.toRadians(lat2));
+    const x = Math.cos(this.toRadians(lat1)) * Math.sin(this.toRadians(lat2)) - Math.sin(this.toRadians(lat1)) * Math.cos(this.toRadians(lat2)) * Math.cos(dLon);
+    return this.toDegrees(Math.atan2(y, x));
   },
 
-  convertHeadingToAFrameCoordinates: function(distance, heading) {
-    const x = distance * Math.sin(heading);
-    const z = distance * Math.cos(heading);
-    return {x: x, y: 0, z: -z};
+  toRadians: function(degrees) {
+    return degrees * (Math.PI / 180);
   },
 
-  toRadians: function(angle) {
-    return angle * (Math.PI / 180);
+  toDegrees: function(radians) {
+    return radians * (180 / Math.PI);
+  },
+
+  convertToAFrameCoords: function(distance, heading) {
+    const x = distance * Math.sin(this.toRadians(heading));
+    const z = distance * Math.cos(this.toRadians(heading));
+    return { x: x, z: -z };
+  }
+});
+
+AFRAME.registerPrimitive('a-geo', {
+  defaultComponents: {
+    'a-geo-comp': {}
   }
 });
