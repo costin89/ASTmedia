d5bd0142d31a3cbfecccab576aef26b8a60c73b6
Alexander Ast <mailtoast@googlemail.com>
2023-08-29 15:30:36 +0200

    Update a-frame-compass.js

--- helper/a-frame-compass.js
+++ helper/a-frame-compass.js
@@ -1,33 +1,41 @@
 // a-frame-compass.js
 AFRAME.registerComponent('a-comp', {
+  schema: {
+    alpha: {type: 'number', default: 0}
+  },
   init: async function() {
     const el = this.el;
 
-    // Funktion zum Setzen der y-Rotation der Box
     const setRotation = (alpha) => {
       if (alpha !== null) {
         el.setAttribute('rotation', {y: 360 - alpha});
+        el.setAttribute('a-comp', 'alpha', alpha);
       }
     };
 
-    // Überprüfen, ob der Browser DeviceOrientationEvents unterstützt
     if ('DeviceOrientationEvent' in window) {
-      // Für iOS 13 und höher
       if (typeof DeviceOrientationEvent.requestPermission === 'function') {
         const permission = await DeviceOrientationEvent.requestPermission();
         if (permission === 'granted') {
           window.addEventListener('deviceorientation', (event) => {
-            // Ständig aktualisierten Wert festlegen
             setRotation(event.alpha);
           });
         }
       } else {
-        // Für andere Browser/Versionen
         window.addEventListener('deviceorientation', (event) => {
-          // Ständig aktualisierten Wert festlegen
           setRotation(event.alpha);
         });
       }
+    }
+  }
+});
+
+AFRAME.registerComponent('alpha-text', {
+  tick: function () {
+    const boxEl = document.querySelector('[a-comp]');
+    if (boxEl) {
+      const alpha = boxEl.getAttribute('a-comp').alpha;
+      this.el.setAttribute('value', `Alpha: ${alpha.toFixed(2)}°`);
     }
   }
 });
