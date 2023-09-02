1c97f89b3a5e62f69b07bf946bd16b6d7d497bc6
Alexander Ast <mailtoast@googlemail.com>
2023-09-02 12:32:49 +0200

    Update a-lignment.js

--- helper/a-lignment.js
+++ helper/a-lignment.js
@@ -1,31 +1,37 @@
-AFRAME.registerComponent('a-ligment', {
-  schema: {
-    north: { type: 'boolean', default: true },
-    deviation: { type: 'number', default: 0 }
-  },
-  
-  init: function () {
-    const el = this.el;
-    const data = this.data;
-    
-    // Popup-Fenster für Berechtigungsanforderung
-    const permissionGranted = window.confirm("Darf diese Anwendung auf die Geräteausrichtung zugreifen?");
-    
-    if (permissionGranted) {
-      if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", function (event) {
-          const alpha = event.alpha;
-          
-          if (data.north) {
-            const adjustedAlpha = alpha + data.deviation;
-            el.object3D.rotation.y = THREE.Math.degToRad(360 - adjustedAlpha);
-          }
-        }, true);
-      } else {
-        console.log("Ihr Gerät unterstützt keine Geräteorientierung.");
-      }
-    } else {
-      console.log("Berechtigung nicht erteilt.");
+AFRAME.registerComponent('a-lignment', {
+    schema: {
+        north: {
+            type: 'boolean',
+            default: true
+        },
+        deviation: {
+            type: 'number',
+            default: 0
+        }
+    },
+
+    init: function() {
+        const el = this.el;
+        const data = this.data;
+
+        // Popup-Fenster für Berechtigungsanforderung
+        const permissionGranted = window.confirm("Darf diese Anwendung auf die Geräteausrichtung zugreifen?");
+
+        if (permissionGranted) {
+            if (window.DeviceOrientationEvent) {
+                window.addEventListener("deviceorientation", function(event) {
+                    const alpha = event.alpha;
+
+                    if (data.north) {
+                        const adjustedAlpha = alpha + data.deviation;
+                        el.object3D.rotation.y = THREE.Math.degToRad(360 - adjustedAlpha);
+                    }
+                }, true);
+            } else {
+                console.log("Ihr Gerät unterstützt keine Geräteorientierung.");
+            }
+        } else {
+            console.log("Berechtigung nicht erteilt.");
+        }
     }
-  }
 });
