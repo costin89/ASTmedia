3b82fe2d21e2842046f581547843478dc7529eea
Alexander Ast <mailtoast@googlemail.com>
2023-09-03 19:34:59 +0200

    Update a-lignment.js

--- helper/a-lignment.js
+++ helper/a-lignment.js
@@ -1,44 +1,61 @@
-AFRAME.registerComponent('a-lignment', {
-    schema: {
-        north: {
-            type: 'boolean',
-            default: true
-        },
-        deviation: {
-            type: 'number',
-            default: 0
-        }
-    },
-
+AFRAME.registerComponent('alignment', {
     init: function() {
-        const el = this.el;
-        const data = this.data;
+        this.handleOrientation = this.handleOrientation.bind(this);
+        this.adjustCompass = this.adjustCompass.bind(this);
+        requestPermission();
+        window.addEventListener('deviceorientation', this.handleOrientation, true);
+    },
+    remove: function() {
+        window.removeEventListener('deviceorientation', this.handleOrientation);
+    },
+    handleOrientation: function(event) {
+        var compassHeading = event.webkitCompassHeading || 360 - event.alpha;
+        var windowOrientation = window.orientation || 0;
+        var adjustedCompassHeading = this.adjustCompass(compassHeading, windowOrientation);
+        var absoluteAngle = event.alpha;
 
-        // Popup für Berechtigungsanforderung
-        const permissionGranted = window.confirm("Darf diese Anwendung auf die Geräteausrichtung zugreifen?");
-
-        if (permissionGranted) {
-            if (window.DeviceOrientationEvent) {
-                window.addEventListener("deviceorientation", function(event) {
-                    const alpha = event.alpha;
-
-                    // Ausgabe des Kompasskurses
-                    if (alpha !== null) {
-                        console.log("Kompasskurs:", alpha);
-                    } else {
-                        console.log("Kompasskurs nicht verfügbar");
-                    }
-
-                    if (data.north && alpha !== null) {
-                        const adjustedAlpha = alpha + data.deviation;
-                        el.object3D.rotation.y = THREE.Math.degToRad(360 - adjustedAlpha);
-                    }
-                }, true);
-            } else {
-                console.log("Ihr Gerät unterstützt keine Geräteorientierung.");
-            }
-        } else {
-            console.log("Berechtigung nicht erteilt.");
+        this.el.setAttribute('rotation', {
+            x: 0,
+            y: adjustedCompassHeading,
+            z: 0
+        });
+    },
+    adjustCompass: function(compassHeading, windowOrientation) {
+        var adjustedCompassHeading = compassHeading;
+        if (windowOrientation === 90) {
+            adjustedCompassHeading = (compassHeading - 90 + 360) % 360;
+        } else if (windowOrientation === -90) {
+            adjustedCompassHeading = (compassHeading + 90) % 360;
         }
+        return adjustedCompassHeading;
     }
 });
+
+async function requestPermission() {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        const permission = await DeviceOrientationEvent.requestPermission();
+        if (permission === 'granted') {
+            init();
+        } else {
+            alert('Berechtigung nicht erteilt');
+        }
+    } else {
+        alert("Diese Anwendung muss auf den Kompass Ihres Geräts zugreifen. Bitte erlauben Sie den Zugriff, wenn Sie dazu aufgefordert werden.");
+        init();
+    }
+}
+
+function init() {
+    if (window.DeviceOrientationEvent) {
+        var scene = document.querySelector('a-scene');
+        if (scene.hasLoaded) {
+            console.log("Scene has loaded");
+        } else {
+            scene.addEventListener('loaded', function() {
+                console.log("Scene has loaded");
+            });
+        }
+    } else {
+        document.getElementById("status").innerHTML = "Ihr Browser unterstützt keine Device Orientation Events.";
+    }
+}
