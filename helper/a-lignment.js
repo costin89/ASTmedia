dbbfd3e01a6e7eb30bc26d75f5bdc2945730459c
Alexander Ast <mailtoast@googlemail.com>
2023-09-03 19:45:38 +0200

    Update a-lignment.js

--- helper/a-lignment.js
+++ helper/a-lignment.js
@@ -1,8 +1,16 @@
 AFRAME.registerComponent('alignment', {
+    schema: {
+        north: {
+            type: 'boolean',
+            default: false
+        }
+    },
     init: function() {
+        if (this.data.north) {
+            requestPermission();
+        }
         this.handleOrientation = this.handleOrientation.bind(this);
         this.adjustCompass = this.adjustCompass.bind(this);
-        requestPermission();
         window.addEventListener('deviceorientation', this.handleOrientation, true);
     },
     remove: function() {
@@ -12,7 +20,6 @@ AFRAME.registerComponent('alignment', {
         var compassHeading = event.webkitCompassHeading || 360 - event.alpha;
         var windowOrientation = window.orientation || 0;
         var adjustedCompassHeading = this.adjustCompass(compassHeading, windowOrientation);
-        var absoluteAngle = event.alpha;
 
         this.el.setAttribute('rotation', {
             x: 0,
@@ -35,27 +42,11 @@ async function requestPermission() {
     if (typeof DeviceOrientationEvent.requestPermission === 'function') {
         const permission = await DeviceOrientationEvent.requestPermission();
         if (permission === 'granted') {
-            init();
+            console.log('Berechtigung erteilt');
         } else {
-            alert('Berechtigung nicht erteilt');
+            console.log('Berechtigung nicht erteilt');
         }
     } else {
-        alert("Diese Anwendung muss auf den Kompass Ihres Geräts zugreifen. Bitte erlauben Sie den Zugriff, wenn Sie dazu aufgefordert werden.");
-        init();
-    }
-}
-
-function init() {
-    if (window.DeviceOrientationEvent) {
-        var scene = document.querySelector('a-scene');
-        if (scene.hasLoaded) {
-            console.log("Scene has loaded");
-        } else {
-            scene.addEventListener('loaded', function() {
-                console.log("Scene has loaded");
-            });
-        }
-    } else {
-        document.getElementById("status").innerHTML = "Ihr Browser unterstützt keine Device Orientation Events.";
+        console.log("Diese Anwendung muss auf den Kompass Ihres Geräts zugreifen. Bitte erlauben Sie den Zugriff, wenn Sie dazu aufgefordert werden.");
     }
 }
